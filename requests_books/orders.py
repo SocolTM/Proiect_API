8af3dc05fcdee62816a603a4516e5065d97096e8
SocolTM <socol.teodora@yahoo.com>
2023-07-07 12:55:47 +0300

    modificare fisier orders

--- requests_books/orders.py
+++ requests_books/orders.py
@@ -1,10 +1,13 @@
 import requests
 from requests.structures import CaseInsensitiveDict
-
-def add_order(token, bookId, customerName):
+def __setHeaders(token):
     headers = CaseInsensitiveDict()
     headers['Accept'] = 'application/json'
     headers['Authorization'] = f'Bearer {token}'
+    return headers
+
+def add_order(token, bookId, customerName):
+    headers = __setHeaders(token)
     json = {
         'bookId': bookId,
         'customerName': customerName
@@ -13,30 +16,22 @@ def add_order(token, bookId, customerName):
     return response
 
 def delete_order(token, orderId):
-    headers = CaseInsensitiveDict()
-    headers['Accept'] = 'application/json'
-    headers['Authorization'] = f'Bearer {token}'
+    headers = __setHeaders(token)
     response = requests.delete(f'https://simple-books-api.glitch.me/orders/{orderId}', headers=headers)
     return response
 
 def get_orders(token):
-    headers = CaseInsensitiveDict()
-    headers['Accept'] = 'application/json'
-    headers['Authorization'] = f'Bearer {token}'
+    headers = __setHeaders(token)
     response = requests.get(f'https://simple-books-api.glitch.me/orders', headers=headers)
     return response
 
 def get_order(token, orderId):
-    headers = CaseInsensitiveDict()
-    headers['Accept'] = 'application/json'
-    headers['Authorization'] = f'Bearer {token}'
+    headers = __setHeaders(token)
     response = requests.get(f'https://simple-books-api.glitch.me/orders/{orderId}', headers=headers)
     return response
 
 def edit_order(token, bookId, customerName):
-    headers = CaseInsensitiveDict()
-    headers['Accept'] = 'application/json'
-    headers['Authorization'] = f'Bearer {token}'
+    headers = __setHeaders(token)
     json = {
         'customerName': customerName
     }
